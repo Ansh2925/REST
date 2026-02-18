5e0b6f158f2c141e7b4f1d54ecd235fd224c5a93
@ns <anshe2925@gmail.com>
2026-02-18 21:54:46 +0530

    DRF setup complete

---
+++ BookListAPI/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['POST'])
+def books(request):
+    return Response('List of books', status=status.HTTP_200_OK)
