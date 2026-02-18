3c6885c9dbdd31b1247fa7b34a64a12cd8f788d7
@ns <anshe2925@gmail.com>
2026-02-19 00:03:07 +0530

    generic views and viewsets in DRF

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -3,6 +3,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 def books(request):
     return Response('List of books', status=status.HTTP_200_OK)
