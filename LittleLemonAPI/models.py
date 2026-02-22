888aecae8bf557e61e8be0a10974858e079d07d5
@ns <anshe2925@gmail.com>
2026-02-22 13:29:18 +0530

    filtering, searching in DRF

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -4,3 +4,14 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=255)
     price = models.IntegerField()
     inventory = models.SmallIntegerField()
+
+    def __str__(self)-> str:
+        return self.title
+
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
+
+    def __str__(self)-> str:
+        return self.title
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import MenuItem
-from .serializers import MenuItemSerializer
+from .models import MenuItem, Category
+from .serializers import MenuItemSerializer, CategorySerializer
 
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+    ordering_fields = ['title', 'inventory']
+
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
+
+
+class CategoriesView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
